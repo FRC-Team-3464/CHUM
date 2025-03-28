f4a0ddc065cbfba29e508997ed5d6b15653b498c
mofoYikes1024 <mofeticyikerson1024@gmail.com>
2025-03-27 20:21:31 -0400

    change auto align name

--- src/main/java/frc/robot/commands/Swerve/AutoAlignReef.java
+++ src/main/java/frc/robot/commands/Swerve/AutoAlignReef.java
@@ -20,6 +20,7 @@ import edu.wpi.first.math.geometry.Transform2d;
 import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.AddressableLED;
@@ -32,25 +33,41 @@ import frc.robot.subsystems.SwerveSubsystem;
 import frc.robot.subsystems.VisionSubsystem;
 
 /* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
-public class AutoCoralPosition extends Command {
-  /** Creates a new AutoCoralPosition. */
+public class AutoAlignReef extends Command {
+  /** Creates a new AutoAlignReef. */
   public final int[] coralIDs = {6, 7, 8, 9, 10, 11, 17, 18, 19, 20, 21, 22};
+
+  private final Map<Integer, Integer> gyroOffsets = Map.ofEntries(
+    Map.entry(6, -60),
+    Map.entry(7,0),
+    Map.entry(8, 60),
+    Map.entry(9, 120),
+    Map.entry(10, 180),
+    Map.entry(11, -120),
+    Map.entry(17,60),
+    Map.entry(18, 0),
+    Map.entry(19, -60),
+    Map.entry(20, -120),
+    Map.entry(21, 180),
+    Map.entry(22, 120),
+    Map.entry(15, 90)
+  );
 
   private double xspeed = 0;
   private double yspeed = 0;
   private double rotationSpeed = 0;
 
-  private static final TrapezoidProfile.Constraints X_CONSTRAINTS = new TrapezoidProfile.Constraints(3, 2);
-  private static final TrapezoidProfile.Constraints Y_CONSTRAINTS = new TrapezoidProfile.Constraints(3, 2);
-  private static final TrapezoidProfile.Constraints THETA_CONSTRAINTS = new TrapezoidProfile.Constraints(3, 2);
+  private static final TrapezoidProfile.Constraints X_CONSTRAINTS = new TrapezoidProfile.Constraints(5, 5);
+  private static final TrapezoidProfile.Constraints Y_CONSTRAINTS = new TrapezoidProfile.Constraints(5, 5);
+  private static final TrapezoidProfile.Constraints THETA_CONSTRAINTS = new TrapezoidProfile.Constraints(3, 3);
 
-  private final ProfiledPIDController xController = new ProfiledPIDController(2, 0, 0, X_CONSTRAINTS);
-  private final ProfiledPIDController yController = new ProfiledPIDController(.5, 0, 0, Y_CONSTRAINTS);
-  private final ProfiledPIDController thetaController = new ProfiledPIDController(.5, 0, 0, THETA_CONSTRAINTS);
+  private final ProfiledPIDController xController = new ProfiledPIDController(7, 0, 0, X_CONSTRAINTS);
+  private final ProfiledPIDController yController = new ProfiledPIDController(7, 0, 0.4, Y_CONSTRAINTS);
+  private final ProfiledPIDController thetaController = new ProfiledPIDController(3, 0, 0, THETA_CONSTRAINTS);
  
   private Transform2d TAG_TO_GOAL = new Transform2d();
 
-  private boolean right;
+  private double yOffset;
 
   public final VisionSubsystem visionSub = VisionSubsystem.getInstance();
   public final SwerveSubsystem swerveSub = SwerveSubsystem.getInstance();
@@ -64,11 +81,11 @@ public class AutoCoralPosition extends Command {
 
   private final PhotonCamera photonCamera;
 
-  public AutoCoralPosition(boolean side) {
+  public AutoAlignReef(boolean side) {
     // Use addRequirements() here to declare subsystem dependencies.
     photonCamera = visionSub.getFrontCamera();
 
-    right = side;
+    yOffset = side ? Units.inchesToMeters(7) : Units.inchesToMeters(-4.5);
 
     xController.setTolerance(0.2);
     yController.setTolerance(0.2);
@@ -77,7 +94,7 @@ public class AutoCoralPosition extends Command {
 
     TAG_TO_GOAL = 
       new Transform2d(
-        new Translation2d(1, side ? Units.inchesToMeters(3) : Units.inchesToMeters(-3)), 
+        new Translation2d(0.4, side ? Units.inchesToMeters(5) : Units.inchesToMeters(-10)), 
         new Rotation3d(0, 0,  Math.PI).toRotation2d()
       );
 
@@ -115,6 +132,9 @@ public class AutoCoralPosition extends Command {
         .filter(t -> t.getPoseAmbiguity() != -1).findFirst();
 
         if (targetOpt.isPresent()) {
+          swerveSub.offsetGyro(gyroOffsets.get(targetOpt.get().getFiducialId()));
+          System.out.println(gyroOffsets.get(targetOpt.get().getFiducialId()));
+
           var target = targetOpt.get();
           lastTarget = target;
   
@@ -125,19 +145,22 @@ public class AutoCoralPosition extends Command {
 
           var goalPose = targetPose.transformBy(TAG_TO_GOAL);
 
-          System.out.println(target.getBestCameraToTarget());
-          // System.out.println("goal: " + goalPose);
+          var goalPoseNew = new Pose2d(camToTarget.getX() + robotPose.getX() - TAG_TO_GOAL.getX(), camToTarget.getY() + robotPose.getY() - yOffset, new Rotation2d(0));
+
+
+          // System.out.println(target.getBestCameraToTarget());
+          System.out.println("goal: " + goalPose);
           // System.out.println("current pose: " + swerveSub.getPose());
-          xController.setGoal(goalPose.getX());
-          yController.setGoal(goalPose.getY());
+          xController.setGoal(goalPoseNew.getX());
+          yController.setGoal(goalPoseNew.getY());
           thetaController.setGoal(goalPose.getRotation().getRadians());   
 
-          if (right) {
-            yspeed = target.getBestCameraToTarget().getY() < (Units.inchesToMeters(4.5)) ? 0.3 : 0;
-          }
-          else {
-            yspeed = target.getBestCameraToTarget().getY() > (Units.inchesToMeters(-4)) ? -0.3 : 0;
-          }
+          // if (right) {
+          //   yspeed = target.getBestCameraToTarget().getY() < (Units.inchesToMeters(4.5)) ? 0.3 : 0;
+          // }
+          // else {
+          //   yspeed = target.getBestCameraToTarget().getY() > (Units.inchesToMeters(-4)) ? -0.3 : 0;
+          // }
           // if (target.getBestCameraToTarget().getRotation().getAngle() < 0) {
           //   rotationSpeed = 0.2;
           // }
@@ -147,7 +170,7 @@ public class AutoCoralPosition extends Command {
           // else {
           //   rotationSpeed = 0;
           // }
-          swerveSub.drive(new Translation2d(0, yspeed), rotationSpeed, false, true);
+          // swerveSub.drive(new Translation2d(0, yspeed), rotationSpeed, false, true);
         }
       }
     }
@@ -159,7 +182,7 @@ public class AutoCoralPosition extends Command {
       var ySpeed = yController.atGoal() ? 0 : yController.calculate(robotPose.getY());
       var thetaSpeed = thetaController.atGoal() ? 0 : thetaController.calculate(robotPose.getRotation().getRadians());
 
-            // swerveSub.drive(new Translation2d(0, ySpeed), 0, false, false);
+      swerveSub.drive(new Translation2d(xSpeed, ySpeed), thetaSpeed, false, false);
     }
     
   }
@@ -168,7 +191,8 @@ public class AutoCoralPosition extends Command {
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    swerveSub.drive(new Translation2d(0, 0), 0, true, true);
+    swerveSub.drive(new Translation2d(0, 0), 0, false, true);
+    swerveSub.offsetGyro(0);
     System.out.println("goal reached");
   }
 
@@ -177,6 +201,9 @@ public class AutoCoralPosition extends Command {
   public boolean isFinished() {
     if (timer.hasElapsed(5)) {
       System.out.println("absolutely nothing in sight, look somewhere else");
+      return true;
+    }
+    if (xController.atGoal() && yController.atGoal()) {
       return true;
     }
     return false;
