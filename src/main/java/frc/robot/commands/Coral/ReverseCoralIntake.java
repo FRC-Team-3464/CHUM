255607e1f7a47798c5974f5cf80b4648c414ed1d
mofoYikes1024 <mofeticyikerson1024@gmail.com>
2025-03-27 20:19:30 -0400

    combien elevator and intake

--- src/main/java/frc/robot/commands/Coral/ReverseCoralIntake.java
+++ src/main/java/frc/robot/commands/Coral/ReverseCoralIntake.java
@@ -4,19 +4,29 @@
 
 package frc.robot.commands.Coral;
 
+import java.util.Map;
+
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Constants.SwerveConstants;
 import frc.robot.subsystems.CoralSubsystem;
+import frc.robot.subsystems.ElevatorSubsystem;
 
 /* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
 public class ReverseCoralIntake extends Command {
  
   private CoralSubsystem coralSub;
+  private ElevatorSubsystem elevatorSub;
+
   private double speed;
+
+
   /** Creates a new IntakeCoral. */
   public ReverseCoralIntake() {
     // Use addRequirements() here to declare subsystem dependencies.
     coralSub = CoralSubsystem.getInstance();
+    elevatorSub = ElevatorSubsystem.getInstance();
+
+    addRequirements(elevatorSub);
     addRequirements(coralSub);
   }
 
@@ -29,13 +39,20 @@ public class ReverseCoralIntake extends Command {
   public void execute() {
     //Tune speed later, through trial & error
 
-    speed = SwerveConstants.targetPosition <= 2 ? 0.5 : -0.5;
+    speed = SwerveConstants.targetPosition == 2 ? -0.9 : 0.5;
+
+    if (SwerveConstants.targetPosition == 4) {
+      elevatorSub.runElevator(0.3);
+    }
+
     coralSub.runIntake(speed);
+
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
+    // elevatorSub.runElevator(0);
     coralSub.runIntake(0);
   }
 
