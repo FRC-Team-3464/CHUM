edff3175245a7519451247a5fe7b89e2f4f1a301
Legion <Legion>
2025-04-29 18:03:59 -0400

    Uncomment Swerve for Merge

--- src/main/java/frc/robot/commands/SwerveCommand.java
+++ src/main/java/frc/robot/commands/SwerveCommand.java
@@ -1,69 +1,69 @@
-// // Copyright (c) FIRST and other WPILib contributors.
-// // Open Source Software; you can modify and/or share it under the terms of
-// // the WPILib BSD license file in the root directory of this project.
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
 
-// package frc.robot.commands;
+package frc.robot.commands;
 
-// import java.util.function.BooleanSupplier;
-// import java.util.function.DoubleSupplier;
+import java.util.function.BooleanSupplier;
+import java.util.function.DoubleSupplier;
 
-// import edu.wpi.first.math.MathUtil;
-// import edu.wpi.first.math.controller.PIDController;
-// import edu.wpi.first.math.geometry.Translation2d;
-// import edu.wpi.first.wpilibj2.command.Command;
-// import frc.robot.Constants;
-// import frc.robot.Constants.SwerveConstants;
-// import frc.robot.subsystems.SwerveSubsystem;
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants;
+import frc.robot.Constants.SwerveConstants;
+import frc.robot.subsystems.SwerveSubsystem;
 
-// public class SwerveCommand extends Command {
-//   private SwerveSubsystem swerveSub;
-//   private PIDController rotationController;
-//   private DoubleSupplier ySpeedSup;
-//   private DoubleSupplier xSpeedSup;
-//   private DoubleSupplier rotationSup;
-//   private BooleanSupplier robotCentricSup;
-//   // private BooleanSupplier dampenSup;
-//   // private DoubleSupplier dynamicHeadingSup;
+public class SwerveCommand extends Command {
+  private SwerveSubsystem swerveSub;
+  private PIDController rotationController;
+  private DoubleSupplier ySpeedSup;
+  private DoubleSupplier xSpeedSup;
+  private DoubleSupplier rotationSup;
+  private BooleanSupplier robotCentricSup;
+  // private BooleanSupplier dampenSup;
+  // private DoubleSupplier dynamicHeadingSup;
 
-//   /** Creates a new SwerveCommand. */
-//   public SwerveCommand(DoubleSupplier ySpeedSup, DoubleSupplier xSpeedSup, DoubleSupplier rotationSup, BooleanSupplier robotCentricSup /* , /*BooleanSupplier dampen, DoubleSupplier dynamicHeadingSup */) {
-//     // Use addRequirements() here to declare subsystem dependencies.
-//     swerveSub = SwerveSubsystem.getInstance();
-//     addRequirements(swerveSub);
+  /** Creates a new SwerveCommand. */
+  public SwerveCommand(DoubleSupplier ySpeedSup, DoubleSupplier xSpeedSup, DoubleSupplier rotationSup, BooleanSupplier robotCentricSup /* , /*BooleanSupplier dampen, DoubleSupplier dynamicHeadingSup */) {
+    // Use addRequirements() here to declare subsystem dependencies.
+    swerveSub = SwerveSubsystem.getInstance();
+    addRequirements(swerveSub);
 
-//     rotationController = new PIDController(Constants.SwerveConstants.headingKP, Constants.SwerveConstants.headingKI, Constants.SwerveConstants.headingKD );
-//     rotationController.enableContinuousInput(-Math.PI, Math.PI);
-//     rotationController.setTolerance(Constants.SwerveConstants.headingTolerence);
+    rotationController = new PIDController(Constants.SwerveConstants.headingKP, Constants.SwerveConstants.headingKI, Constants.SwerveConstants.headingKD );
+    rotationController.enableContinuousInput(-Math.PI, Math.PI);
+    rotationController.setTolerance(Constants.SwerveConstants.headingTolerence);
 
-//     this.ySpeedSup = ySpeedSup;
-//     this.xSpeedSup = xSpeedSup;
-//     this.rotationSup = rotationSup;
-//     this.robotCentricSup = robotCentricSup;
-//     // this.dampenSup = dampen;
-//     // this.dynamicHeadingSup = dynamicHeadingSup;
-//   }
-//   // Called when the command is initially scheduled.
-//   @Override
-//   public void initialize() {}
+    this.ySpeedSup = ySpeedSup;
+    this.xSpeedSup = xSpeedSup;
+    this.rotationSup = rotationSup;
+    this.robotCentricSup = robotCentricSup;
+    // this.dampenSup = dampen;
+    // this.dynamicHeadingSup = dynamicHeadingSup;
+  }
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
 
-//   // Called every time the scheduler runs while the command is scheduled.
-//   @Override
-//   public void execute() {
-//     double xSpeed = MathUtil.applyDeadband(xSpeedSup.getAsDouble(), Constants.SwerveConstants.kDeadband);
-//     double ySpeed = MathUtil.applyDeadband(ySpeedSup.getAsDouble(), Constants.SwerveConstants.kDeadband);
-//     double rotation = MathUtil.applyDeadband(rotationSup.getAsDouble(), Constants.SwerveConstants.kDeadband);
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    double xSpeed = MathUtil.applyDeadband(xSpeedSup.getAsDouble(), Constants.SwerveConstants.kDeadband);
+    double ySpeed = MathUtil.applyDeadband(ySpeedSup.getAsDouble(), Constants.SwerveConstants.kDeadband);
+    double rotation = MathUtil.applyDeadband(rotationSup.getAsDouble(), Constants.SwerveConstants.kDeadband);
 
-//     rotation *= Constants.SwerveConstants.maxAngularVelocity;
-//             swerveSub.drive(new Translation2d(xSpeed, ySpeed).times(SwerveConstants.maxSpeed), rotation, robotCentricSup.getAsBoolean(), true);
-//   }
+    rotation *= Constants.SwerveConstants.maxAngularVelocity;
+            swerveSub.drive(new Translation2d(xSpeed, ySpeed).times(SwerveConstants.maxSpeed), rotation, robotCentricSup.getAsBoolean(), true);
+  }
 
-//   // Called once the command ends or is interrupted.
-//   @Override
-//   public void end(boolean interrupted) {}
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
 
-//   // Returns true when the command should end.
-//   @Override
-//   public boolean isFinished() {
-//     return false;
-//   }
-// }
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
--- src/main/java/frc/robot/subsystems/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/SwerveSubsystem.java
@@ -1,170 +1,170 @@
-// // Copyright (c) FIRST and other WPILib contributors.
-// // Open Source Software; you can modify and/or share it under the terms of
-// // the WPILib BSD license file in the root directory of this project.
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
 
-// package frc.robot.subsystems;
+package frc.robot.subsystems;
 
-// import edu.wpi.first.wpilibj.DriverStation;
-// import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Timer;
 
-// import com.pathplanner.lib.auto.AutoBuilder;
-// import com.pathplanner.lib.config.PIDConstants;
-// import com.pathplanner.lib.config.RobotConfig;
-// import com.pathplanner.lib.controllers.PPHolonomicDriveController;
-// import com.studica.frc.AHRS;
-// import com.studica.frc.AHRS.NavXComType;
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.config.PIDConstants;
+import com.pathplanner.lib.config.RobotConfig;
+import com.pathplanner.lib.controllers.PPHolonomicDriveController;
+import com.studica.frc.AHRS;
+import com.studica.frc.AHRS.NavXComType;
 
-// import edu.wpi.first.math.geometry.Pose2d;
-// import edu.wpi.first.math.geometry.Rotation2d;
-// import edu.wpi.first.math.geometry.Translation2d;
-// import edu.wpi.first.math.kinematics.ChassisSpeeds;
-// import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
-// import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
-// import edu.wpi.first.math.kinematics.SwerveModulePosition;
-// import edu.wpi.first.math.kinematics.SwerveModuleState;
-// import edu.wpi.first.wpilibj2.command.SubsystemBase;
-// import frc.robot.Constants;
-// import frc.robot.Constants.SwerveConstants;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import frc.robot.Constants.SwerveConstants;
 
-// public class SwerveSubsystem extends SubsystemBase {
-//   /** Creates a new SwerveSubsystem. */
-//   public static SwerveSubsystem instance;
-//   public SwerveDriveOdometry swerveOdometry;
-//   public SwerveModule[] swerveMods;
-//   public AHRS gyro;
+public class SwerveSubsystem extends SubsystemBase {
+  /** Creates a new SwerveSubsystem. */
+  public static SwerveSubsystem instance;
+  public SwerveDriveOdometry swerveOdometry;
+  public SwerveModule[] swerveMods;
+  public AHRS gyro;
   
-//   public SwerveSubsystem() {
-//     gyro = new AHRS(NavXComType.kMXP_SPI);
-//     gyro.zeroYaw();
+  public SwerveSubsystem() {
+    gyro = new AHRS(NavXComType.kMXP_SPI);
+    gyro.zeroYaw();
 
-//     swerveMods = new SwerveModule[] {
-//       new SwerveModule(0, Constants.ModConstants.Mod0.constants),
-//       new SwerveModule(1, Constants.ModConstants.Mod1.constants),
-//       new SwerveModule(2, Constants.ModConstants.Mod2.constants),
-//       new SwerveModule(3, Constants.ModConstants.Mod3.constants)
-//     };
+    swerveMods = new SwerveModule[] {
+      new SwerveModule(0, Constants.ModConstants.Mod0.constants),
+      new SwerveModule(1, Constants.ModConstants.Mod1.constants),
+      new SwerveModule(2, Constants.ModConstants.Mod2.constants),
+      new SwerveModule(3, Constants.ModConstants.Mod3.constants)
+    };
 
-//     Timer.delay(1);
-//     resetModulesToAbsolute();
+    Timer.delay(1);
+    resetModulesToAbsolute();
 
-//     swerveOdometry = new SwerveDriveOdometry(SwerveConstants.swerveKinematics, getYaw(), getModulePositions());
+    swerveOdometry = new SwerveDriveOdometry(SwerveConstants.swerveKinematics, getYaw(), getModulePositions());
 
-//     RobotConfig config;
-//     try{
-//       config = RobotConfig.fromGUISettings();
-//     } catch (Exception e) {
-//       // Handle exception as needed
-//       e.printStackTrace();
-//     }
+    RobotConfig config;
+    try{
+      config = RobotConfig.fromGUISettings();
+    } catch (Exception e) {
+      // Handle exception as needed
+      e.printStackTrace();
+    }
 
-//      AutoBuilder.configure(
-//             this::getPose, // Robot pose supplier
-//             this::resetPose, // Method to reset odometry (will be called if your auto has a starting pose)
-//             this::getRobotRelativeSpeeds, // ChassisSpeeds supplier. MUST BE ROBOT RELATIVE
-//             (speeds, feedforwards) -> driveRobotRelative(speeds), // Method that will drive the robot given ROBOT RELATIVE ChassisSpeeds. Also optionally outputs individual module feedforwards
-//             new PPHolonomicDriveController( // PPHolonomicController is the built in path following controller for holonomic drive trains
-//                     new PIDConstants(5.0, 0.0, 0.0), // Translation PID constants
-//                     new PIDConstants(5.0, 0.0, 0.0) // Rotation PID constants
-//             ),
-//             config, // The robot configuration
-//             () -> {
-//               // Boolean supplier that controls when the path will be mirrored for the red alliance
-//               // This will flip the path being followed to the red side of the field.
-//               // THE ORIGIN WILL REMAIN ON THE BLUE SIDE
+     AutoBuilder.configure(
+            this::getPose, // Robot pose supplier
+            this::resetPose, // Method to reset odometry (will be called if your auto has a starting pose)
+            this::getRobotRelativeSpeeds, // ChassisSpeeds supplier. MUST BE ROBOT RELATIVE
+            (speeds, feedforwards) -> driveRobotRelative(speeds), // Method that will drive the robot given ROBOT RELATIVE ChassisSpeeds. Also optionally outputs individual module feedforwards
+            new PPHolonomicDriveController( // PPHolonomicController is the built in path following controller for holonomic drive trains
+                    new PIDConstants(5.0, 0.0, 0.0), // Translation PID constants
+                    new PIDConstants(5.0, 0.0, 0.0) // Rotation PID constants
+            ),
+            config, // The robot configuration
+            () -> {
+              // Boolean supplier that controls when the path will be mirrored for the red alliance
+              // This will flip the path being followed to the red side of the field.
+              // THE ORIGIN WILL REMAIN ON THE BLUE SIDE
 
-//               var alliance = DriverStation.getAlliance();
-//               if (alliance.isPresent()) {
-//                 return alliance.get() == DriverStation.Alliance.Red;
-//               }
-//               return false;
-//             },
-//             this // Reference to this subsystem to set requirements
-//     );
+              var alliance = DriverStation.getAlliance();
+              if (alliance.isPresent()) {
+                return alliance.get() == DriverStation.Alliance.Red;
+              }
+              return false;
+            },
+            this // Reference to this subsystem to set requirements
+    );
 
-//   }
+  }
 
-//   public static SwerveSubsystem getInstance() {
-//     if (instance == null) {
-//       instance = new SwerveSubsystem();
-//     }
-//     return instance;
-//   }
+  public static SwerveSubsystem getInstance() {
+    if (instance == null) {
+      instance = new SwerveSubsystem();
+    }
+    return instance;
+  }
  
-//   public void drive(Translation2d translation, double rotation, boolean fieldRelative, boolean isOpenLoop) {
-//     SwerveModuleState[] swerveModuleStates =
-//         Constants.SwerveConstants.swerveKinematics.toSwerveModuleStates(
-//             fieldRelative ? ChassisSpeeds.fromFieldRelativeSpeeds(
-//                                 translation.getX(), 
-//                                 translation.getY(), 
-//                                 rotation, 
-//                                 getYaw()
-//                             )
-//                             : new ChassisSpeeds(
-//                                 translation.getX(), 
-//                                 translation.getY(), 
-//                                 rotation)
-//                             );
-//     SwerveDriveKinematics.desaturateWheelSpeeds(swerveModuleStates, Constants.SwerveConstants.maxSpeed);
+  public void drive(Translation2d translation, double rotation, boolean fieldRelative, boolean isOpenLoop) {
+    SwerveModuleState[] swerveModuleStates =
+        Constants.SwerveConstants.swerveKinematics.toSwerveModuleStates(
+            fieldRelative ? ChassisSpeeds.fromFieldRelativeSpeeds(
+                                translation.getX(), 
+                                translation.getY(), 
+                                rotation, 
+                                getYaw()
+                            )
+                            : new ChassisSpeeds(
+                                translation.getX(), 
+                                translation.getY(), 
+                                rotation)
+                            );
+    SwerveDriveKinematics.desaturateWheelSpeeds(swerveModuleStates, Constants.SwerveConstants.maxSpeed);
 
-//     for(SwerveModule mod : swerveMods){
-//         mod.setDesiredState(swerveModuleStates[mod.moduleNumber], isOpenLoop);
-//     }
-// }    
+    for(SwerveModule mod : swerveMods){
+        mod.setDesiredState(swerveModuleStates[mod.moduleNumber], isOpenLoop);
+    }
+}    
 
-//   public SwerveModulePosition[] getModulePositions() {
-//     SwerveModulePosition[] positions = new SwerveModulePosition[4];
-//     for(SwerveModule mod : swerveMods) {
-//         positions[mod.getModuleNumber()] = mod.getPosition();
-//     }
-//     return positions;
-//   }
+  public SwerveModulePosition[] getModulePositions() {
+    SwerveModulePosition[] positions = new SwerveModulePosition[4];
+    for(SwerveModule mod : swerveMods) {
+        positions[mod.getModuleNumber()] = mod.getPosition();
+    }
+    return positions;
+  }
 
-//   public SwerveModuleState[] getModuleStates(){
-//     SwerveModuleState[] states = new SwerveModuleState[4];
-//     for(SwerveModule mod : swerveMods){
-//         states[mod.moduleNumber] = mod.getState();
-//     }
-//     return states;
-//   }
+  public SwerveModuleState[] getModuleStates(){
+    SwerveModuleState[] states = new SwerveModuleState[4];
+    for(SwerveModule mod : swerveMods){
+        states[mod.moduleNumber] = mod.getState();
+    }
+    return states;
+  }
 
-//   public Pose2d getPose() {
-//     return swerveOdometry.getPoseMeters();
-//   }
+  public Pose2d getPose() {
+    return swerveOdometry.getPoseMeters();
+  }
 
-//   public void resetPose(Pose2d pose) {
-//     swerveOdometry.resetPosition(getYaw(), getModulePositions(), pose);
-//   }
+  public void resetPose(Pose2d pose) {
+    swerveOdometry.resetPosition(getYaw(), getModulePositions(), pose);
+  }
 
-//   public ChassisSpeeds getRobotRelativeSpeeds(){
-//     return Constants.SwerveConstants.swerveKinematics.toChassisSpeeds(getModuleStates());
-//   }
+  public ChassisSpeeds getRobotRelativeSpeeds(){
+    return Constants.SwerveConstants.swerveKinematics.toChassisSpeeds(getModuleStates());
+  }
 
-//   public void setModuleStates(SwerveModuleState[] desiredStates) {
-//     SwerveDriveKinematics.desaturateWheelSpeeds(desiredStates, Constants.SwerveConstants.maxSpeed);
+  public void setModuleStates(SwerveModuleState[] desiredStates) {
+    SwerveDriveKinematics.desaturateWheelSpeeds(desiredStates, Constants.SwerveConstants.maxSpeed);
     
-//     for(SwerveModule mod : swerveMods){
-//         mod.setDesiredState(desiredStates[mod.moduleNumber], false);
-//     }
-//   }    
+    for(SwerveModule mod : swerveMods){
+        mod.setDesiredState(desiredStates[mod.moduleNumber], false);
+    }
+  }    
 
-//   public void driveRobotRelative(ChassisSpeeds speeds){
-//     SwerveModuleState[] states = Constants.SwerveConstants.swerveKinematics.toSwerveModuleStates(speeds);
-//     SwerveDriveKinematics.desaturateWheelSpeeds(states, Constants.SwerveConstants.maxSpeed);
-//     setModuleStates(states);
-//   }
+  public void driveRobotRelative(ChassisSpeeds speeds){
+    SwerveModuleState[] states = Constants.SwerveConstants.swerveKinematics.toSwerveModuleStates(speeds);
+    SwerveDriveKinematics.desaturateWheelSpeeds(states, Constants.SwerveConstants.maxSpeed);
+    setModuleStates(states);
+  }
 
-//   public void resetModulesToAbsolute(){
-//     for(SwerveModule mod : swerveMods){
-//         mod.resetToAbsolute();
-//     }
-//   }
+  public void resetModulesToAbsolute(){
+    for(SwerveModule mod : swerveMods){
+        mod.resetToAbsolute();
+    }
+  }
 
-//   public Rotation2d getYaw() {
-//     return gyro.getRotation2d();
-//   }
+  public Rotation2d getYaw() {
+    return gyro.getRotation2d();
+  }
 
-//   @Override
-//   public void periodic() {
-//     // This method will be called once per scheduler run
-//   }
-// }
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
