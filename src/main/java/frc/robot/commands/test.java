516d3eca4e92bd089d2a104e4af48e1e26de5152
Legion <Legion>
2025-05-08 19:21:20 -0400

    trying to get throttle to work (it didnt ask ben or peiwei for help)

--- src/main/java/frc/robot/commands/test.java
+++ src/main/java/frc/robot/commands/test.java
@@ -2,24 +2,15 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.commands.Climber;
+package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.Constants.OperatorConstants;
-import frc.robot.subsystems.ClimbSubsystem;
 
 /* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
-public class LowerClimber extends Command {
-  /** Creates a new LowerClimber. */
-
-  private ClimbSubsystem climberSub;
-
-  public LowerClimber() {
+public class test extends Command {
+  /** Creates a new test. */
+  public test() {
     // Use addRequirements() here to declare subsystem dependencies.
-
-    climberSub = ClimbSubsystem.getInstance();
-
-    addRequirements(climberSub);
   }
 
   // Called when the command is initially scheduled.
@@ -28,16 +19,11 @@ public class LowerClimber extends Command {
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {
-    //climberSub.LowerClimber(0.5);
-    System.out.println(String.valueOf(OperatorConstants.auxStick.getThrottle()));
-  }
+  public void execute() {}
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {
-    //climberSub.StopClimber();
-  }
+  public void end(boolean interrupted) {}
 
   // Returns true when the command should end.
   @Override
